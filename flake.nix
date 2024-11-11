f95b1fd82d31f3ca140e6b9484b0bcd97194909b
ryane <ryanesc@gmail.com>
2024-11-11 07:22:43 -0500

    flake: "simplify" and fix flake configuration

--- flake.nix
+++ flake.nix
@@ -1,82 +1,57 @@
 {
   description = "kustomize-gopass: Generate secrets from gopass values.";
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      # System types to support.
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-    in
     {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          kustomize-gopass = pkgs.buildGoModule {
-            name = "kustomize-gopass";
-            pname = "kustomize-gopass";
+        app = pkgs.buildGo123Module {
+          name = "kustomize-gopass";
+          pname = "kustomize-gopass";
 
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
-            src = ./.;
+          src = ./.;
 
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake hash and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remember to bump this hash when your dependencies change.
-            # vendorHash = pkgs.lib.fakeHash;
+          # This hash locks the dependencies of this package. It is
+          # necessary because of how Go requires network access to resolve
+          # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+          # details. Normally one can build with a fake hash and rely on native Go
+          # mechanisms to tell you what the hash should be or determine what
+          # it should be "out-of-band" with other tooling (eg. gomod2nix).
+          # To begin with it is recommended to set this, but one must
+          # remember to bump this hash when your dependencies change.
+          # vendorHash = pkgs.lib.fakeHash;
+          vendorHash = "sha256-Vv42dLN0iKJUEL1o0ZxCHq741xufXHiakw97eVbkcxo=";
+        };
 
-            vendorHash = "sha256-Vv42dLN0iKJUEL1o0ZxCHq741xufXHiakw97eVbkcxo=";
-          };
-        }
-      );
-
-      # Add dependencies that are only needed for development
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              go
-              gopls
-              gotools
-              go-tools
-
-              kustomize
-            ];
-          };
-        }
-      );
-
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.kustomize-gopass);
-    };
+      in
+      with pkgs;
+      {
+        packages = {
+          default = app;
+        };
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            go_1_23
+            gopls
+            gotools
+            go-tools
+            kustomize
+          ];
+        };
+        overlays = final: prev: { kustomize-gopass = app; };
+      }
+    );
 }
